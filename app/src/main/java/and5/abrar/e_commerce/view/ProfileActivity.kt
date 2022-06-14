197b17164ab37fcffe396489cc5e95cf9bc3bfd2
Waslim <waslim.liem19240@student.unsika.ac.id>
2022-06-14 22:31:22 +0700

    add layout profile

---
+++ app/src/main/java/and5/abrar/e_commerce/view/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package and5.abrar.e_commerce.view
+
+import and5.abrar.e_commerce.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
