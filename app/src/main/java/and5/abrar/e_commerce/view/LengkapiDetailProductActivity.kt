e784cc6415124e7a9f7c80765c194b9b4ff27973
Lukman <lukman081217@gmail.com>
2022-06-14 22:37:00 +0700

    Lengkapi Detail product

---
+++ app/src/main/java/and5/abrar/e_commerce/view/LengkapiDetailProductActivity.kt
@@ -0,0 +1,12 @@
+package and5.abrar.e_commerce.view
+
+import and5.abrar.e_commerce.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LengkapiDetailProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lengkapi_detail_product)
+    }
+}
