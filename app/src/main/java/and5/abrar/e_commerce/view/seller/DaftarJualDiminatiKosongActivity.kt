c5272fa9e75b81611e943d54b3e5dc394108bc49
Waslim <waslim.liem19240@student.unsika.ac.id>
2022-06-15 23:47:15 +0700

    add layout daftar diminati kosong

---
+++ app/src/main/java/and5/abrar/e_commerce/view/seller/DaftarJualDiminatiKosongActivity.kt
@@ -0,0 +1,12 @@
+package and5.abrar.e_commerce.view.seller
+
+import and5.abrar.e_commerce.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DaftarJualDiminatiKosongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daftar_jual_dua_diminati_kosong)
+    }
+}
